88cf2aa98c392f2382e731eba5c39cfa84dc0579
miracle_tian <858887914@qq.com>
2022-11-28 00:01:18 +0800

    update

---
+++ inorder_traversal_94/src/lib.rs
@@ -0,0 +1,68 @@
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+struct Solution;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut res = Vec::new();
+        Self::inorder(&root, &mut res);
+        res
+    }
+
+    fn inorder(node: &Option<Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
+        if let Some(node) = node {
+            Self::inorder(&node.borrow().left, res);
+
+            res.push(node.borrow().val);
+            Self::inorder(&node.borrow().right, res)
+        }
+    }
+
+    pub fn inorder_traversal1(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut ans = vec![];
+        let mut stack = vec![];
+        let mut node = root;
+
+        while node.is_some() || stack.len() > 0 {
+            while let Some(n) = node {
+                node = n.borrow_mut().left.take();
+                stack.push(n);
+            }
+
+            if let Some(n) = stack.pop() {
+                ans.push(n.borrow().val);
+                node = n.borrow_mut().right.take();
+            }
+        }
+
+        ans
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
