04e4a1ba654bf1aacf7a106660f1f87fa4814002
miracle_tian <858887914@qq.com>
2022-12-03 16:23:33 +0800

    update

--- has_cycle_141/src/lib.rs
+++ has_cycle_141/src/lib.rs
@@ -1,6 +1,6 @@
 use std::ptr::NonNull;
 
-
+#[derive(PartialEq)]
 pub struct ListNodeNonNull {
     pub val: i32,
     pub next: Option<NonNull<ListNodeNonNull>>,
@@ -10,6 +10,20 @@ impl ListNodeNonNull {
     #[inline]
     fn new(val: i32) -> Self {
         ListNodeNonNull { next: None, val }
+    }
+}
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
 }
 
@@ -32,7 +46,77 @@ impl Solution {
             false
         }
     }
+
+    pub fn has_cycle1(head: Option<NonNull<ListNodeNonNull>>) -> bool {
+        unsafe {
+            if head.is_none() || (*head.unwrap().as_ptr()).next.is_none() {
+                return false;
+            }
+
+            let mut slow = head;
+            let mut fast = (*head.unwrap().as_ptr()).next;
+
+            while slow != fast {
+                if fast.is_none() || (*fast.unwrap().as_ptr()).next.is_none() {
+                    return false;
+                }
+                slow = (*slow.unwrap().as_ptr()).next;
+                fast = (*fast.unwrap().as_ptr()).next;
+                fast = (*fast.unwrap().as_ptr()).next;
+            }
+
+            return true;
+        }
+    }
+
+    pub fn has_cycle2(head: Option<Box<ListNode>>) -> bool {
+        if head.is_none() || head.as_ref().unwrap().next.is_none() {
+            return false;
+        }
+
+        let mut slow = head.as_ref();
+        let mut fast = head.as_ref().unwrap().next.as_ref();
+
+        while slow != fast {
+            if fast.is_none() || fast.unwrap().next.is_none() {
+                return false;
+            }
+            slow = slow.unwrap().next.as_ref();
+            fast = fast.unwrap().next.as_ref();
+            fast = fast.unwrap().next.as_ref();
+        }
+
+        return true;
+    }
 }
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// func hasCycle(head *ListNode) bool {
+
+//     if head == nil || head.Next == nil{
+//         return false
+//     }
+
+//     slow := head
+//     fast := head.Next
+
+//     for slow != fast{
+//         if fast ==nil || fast.Next==nil {
+//             return false
+//         }
+//         slow = slow.Next
+//         fast = fast.Next.Next
+
+//     }
+
+//     return true
+// }
 
 #[cfg(test)]
 mod tests {
