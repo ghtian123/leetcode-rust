76e564013a5db5e0b03bc06460c1ec15403a457d
miracle_tian <858887914@qq.com>
2022-12-11 14:37:09 +0800

    update

--- lru_146/src/lib.rs
+++ lru_146/src/lib.rs
@@ -1,35 +1,263 @@
-struct LRUCache {}
+use std::collections::HashMap;
+use std::ptr::drop_in_place;
+use std::{marker::PhantomData, ptr::NonNull};
 
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
+//ref : https://zhuanlan.zhihu.com/p/466409120
+pub struct Node<T: Default> {
+    ele: T,
+    prev: Option<NonNull<Node<T>>>,
+    next: Option<NonNull<Node<T>>>,
+}
+
+impl<T: Default> Node<T> {
+    fn new(elem: T) -> Self {
+        Self {
+            ele: elem,
+            prev: None,
+            next: None,
+        }
+    }
+}
+
+pub struct LinkedList<T: Default> {
+    head: Option<NonNull<Node<T>>>,
+    tail: Option<NonNull<Node<T>>>,
+    // 幽灵数据，本质上是一个标记。因为LinkedList使用了NonNull，类似裸指针，
+    // 所以LinkedList和T之间的关系就有点模糊不清了。
+    // 比如LinkedList析构的时候是否需要析构T，
+    // 如果把LinkedList使用默认的析构函数，
+    // 那么T肯定没有被析构，存在内存泄露。
+    // 所以使用LinkedList的人就会比较迷惑，所以需要加个标记，
+    // 标记LinkedList拥有T，即LinkedList析构，T也就析构了，
+    // 同理T的生命周期不可能超过LinkedList，这里的不超过指的是生命周期的结束点
+    marker: PhantomData<T>,
+}
+
+impl<T: Default> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+            marker: PhantomData,
+        }
+    }
+    pub fn push_back(&mut self, ele: T) {
+        // 这里使用Box::new创建一个堆对象，
+        // 然后通过Box::leak主动将Box对象泄露出去，
+        // 因为Box类似unique_ptr，离开作用域就析构了
+        // 所以Box::leak(Box::new) 就等效于C++中的new，如果不主动释放则存在内存泄露
+        let node = Box::leak(Box::new(Node {
+            ele,
+            prev: self.tail,
+            next: None,
+        }))
+        .into();
+        // 这里和之前的safe代码很像了。
+        // 如果函数有返回值，推荐使用Option::map方法。
+        // 如果函数没有返回值，推荐使用match匹配Option。
+        // 这里为什么没有使用 match self.tail.take呢？
+        // 是不是self.tail所有权被移走了呢？
+        // 其实这里是因为NonNull具有Copy语义，复制了。
+        // 如果T是Copy语义，Option<T>也具备Copy语义
+        match self.tail {
+            Some(mut tail) => unsafe {
+                tail.as_mut().next = Some(node);
+                self.tail = Some(node);
+            },
+            None => {
+                self.head = Some(node);
+                self.tail = Some(node);
+            }
+        }
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        // 如push_back函数说明一样，这里采用Option::map方法
+        // 主意NonNull的as_mut方法的签名是pub unsafe fn as_mut<'a>(&mut self) -> &'a mut T
+        // 所以需要NonNull是可变的，所以在map的闭包中的参数是mut
+        self.tail.take().map(|mut tail| unsafe {
+            match tail.as_mut().prev {
+                Some(mut prev) => {
+                    prev.as_mut().next = None;
+                    tail.as_mut().prev = None;
+                    self.tail = Some(prev);
+                }
+                None => {
+                    self.head = None;
+                    self.tail = None;
+                }
+            }
+            // 这里特别要主意一下，因为是pop_back，
+            // 所以需要主动释放对应Node的内存
+            // 这里就类似让unique_ptr重新接管裸指针，
+            // 离开作用域自动析构
+            let mut node = Box::from_raw(tail.as_ptr());
+            // 这里把node的ele成员所有权转移走了
+            // 为什么这里可以转移某个变量的成员呢？
+            // 之前写Safe Rust的时候，self.tail的所有权为什么无法转移呢？
+            // 这是因为node已经没有地方再使用了，而且node没有自定义析构函数。
+            // 可以认为ele成员没有任何地方再使用了，所以可以安全被转移走了。
+
+            std::mem::replace(&mut node.ele, T::default())
+            // node.ele
+        })
+    }
+
+    pub fn peek_back(&self) -> Option<&T> {
+        // 这里的peek_back就比较简单了，而且返回值更加自然Option<&T>，
+        // 而Safe Code只能返回Option<Ref<T>>，原因见Safe Code代码说明
+        self.tail.map(|node| unsafe { &node.as_ref().ele })
+    }
+
+    pub fn remove_node_from_list(&mut self, node: Option<NonNull<Node<T>>>) {
+        if let Some(mut n) = node {
+            unsafe {
+                let mut prev = (*n.as_ptr()).prev.take();
+                let mut next = (*n.as_ptr()).next.take();
+                if let Some(p) = &mut prev {
+                    (*p.as_ptr()).next = next;
+                } else {
+                    self.head = next;
+                }
+
+                if let Some(n) = &mut next {
+                    n.as_mut().prev = prev;
+                } else {
+                    self.tail = prev;
+                }
+            }
+        }
+    }
+
+    pub fn push_node_back(&mut self, mut node: Option<NonNull<Node<T>>>) {
+        unsafe {
+            if let Some(mut t) = self.tail.take() {
+                if let Some(n) = &mut node {
+                    (*t.as_ptr()).next = Some(*n);
+                    (*n.as_ptr()).prev = Some(t);
+                }
+            } else {
+                self.head = node;
+            }
+
+            self.tail = node;
+        }
+    }
+
+    pub fn pop_front_node(&mut self) -> Option<NonNull<Node<T>>> {
+        unsafe {
+            match self.head.take() {
+                Some(mut head) => {
+                    if let Some(mut n) = (*head.as_ptr()).next.take() {
+                        (*n.as_ptr()).prev = None;
+                        self.head = Some(n);
+                    } else {
+                        self.head = None;
+                        self.tail = None;
+                    }
+
+                    Some(head)
+                }
+                None => None,
+            }
+        }
+    }
+}
+
+impl<T: Default> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        while self.pop_back().is_some() {}
+    }
+}
+
+// NonNull实现了Copy语义
+// 可以通过Box::leak(Box::new).into生成NonNull，类似C++中的new
+// NonNull的as_mut方法需要NonNull自身是可变的
+// NonNull需要手动释放内存，可以通过Box::from_raw(xx.as_ptr())，让智能指针重新接管裸指针
+struct LRUCacheI32<T: Default> {
+    cap: usize,
+    used: usize,
+    data: HashMap<i32, NonNull<Node<T>>>,
+    list: LinkedList<T>,
+}
+
+impl LRUCacheI32<(i32, i32)> {
+    fn new(capacity: i32) -> Self {
+        Self {
+            cap: capacity as usize,
+            used: 0,
+            data: HashMap::new(),
+            list: LinkedList::new(),
+        }
+    }
+
+    fn get(&mut self, key: i32) -> i32 {
+        if let Some(node) = self.data.get(&key) {
+            self.list.remove_node_from_list(Some(*node));
+
+            self.list.push_node_back(Some(*node));
+
+            unsafe {
+                let val = (*node.as_ptr()).ele.1;
+
+                val
+            }
+        } else {
+            -1
+        }
+    }
+
+    fn put(&mut self, key: i32, value: i32) {
+        unsafe {
+            if let Some(node) = self.data.get_mut(&key) {
+                node.as_mut().ele = (key, value);
+
+                self.list.remove_node_from_list(Some(*node));
+
+                self.list.push_node_back(Some(*node))
+            } else {
+                if self.used >= self.cap {
+                    if let Some(node) = self.list.pop_front_node() {
+                        self.data.remove(&node.as_ref().ele.0);
+
+                        drop_in_place(node.as_ptr());
+                        // let node = Box::from_raw();
+                        // // //释放内存
+                        // drop(node);
+                        self.used -= 1;
+                    }
+                }
+
+                let new_node = Box::leak(Box::new(Node::new((key, value)))).into();
+
+                self.data.insert(key, new_node);
+
+                self.list.push_node_back(Some(new_node));
+                self.used += 1;
+            }
+        }
+    }
+}
+
+struct LRUCache {
+    cache: LRUCacheI32<(i32, i32)>,
+}
+
 impl LRUCache {
     fn new(capacity: i32) -> Self {
-        todo!()
+        Self {
+            cache: LRUCacheI32::new(capacity),
+        }
     }
 
-    fn get(&self, key: i32) -> i32 {
-        todo!()
+    fn get(&mut self, key: i32) -> i32 {
+        self.cache.get(key)
     }
 
-    fn put(&self, key: i32, value: i32) {
-        todo!()
-    }
-}
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * let obj = LRUCache::new(capacity);
- * let ret_1: i32 = obj.get(key);
- * obj.put(key, value);
- */
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn put(&mut self, key: i32, value: i32) {
+        self.cache.put(key, value)
     }
 }
+
+#[test]
+fn test() {}
---
+++ lru_safe_146/src/lib.rs
@@ -0,0 +1,225 @@
+use std::cell::{Ref, RefCell};
+use std::collections::HashMap;
+use std::rc::Rc;
+
+//ref : https://zhuanlan.zhihu.com/p/466409120
+
+pub struct Node<T> {
+    ele: T,
+    prev: Option<Rc<RefCell<Node<T>>>>,
+    next: Option<Rc<RefCell<Node<T>>>>,
+}
+
+impl<T> Node<T> {
+    fn new(ele: T) -> Self {
+        Self {
+            ele,
+            prev: None,
+            next: None,
+        }
+    }
+}
+
+pub struct LinkedList<T> {
+    head: Option<Rc<RefCell<Node<T>>>>,
+    tail: Option<Rc<RefCell<Node<T>>>>,
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push_back(&mut self, ele: T) {
+        // 创建node，不需要设置为mut
+        // 因为prev, next字段都由RefCell包裹，
+        // 所以prev, next都是可变的
+        let node = Rc::new(RefCell::new(Node::new(ele)));
+        // 这里要特别主意一下
+        // 因为match会导致所有权转移
+        // tail是属于self的字段，rust不允许所有权被转移走
+        // 这里使用Option的take方法，把内部值转移走，而self.tail变为None
+        match self.tail.take() {
+            Some(tail) => {
+                // borror_mut是RefCell的方法，让内部的值变为可变
+                // Rc没有实现Copy Trait，但是实现clone方法，不过需要手动调用一下该方法
+                // 代码逻辑比较简单了: 如果tail存在则往后追加节点，并把节点链接起来
+                tail.borrow_mut().next = Some(node.clone());
+                node.borrow_mut().prev = Some(tail);
+                self.tail = Some(node);
+            }
+            None => {
+                // 如果self.tail是None表示第一次push，则更新一下self.head
+                // 因为双向链表只有一个值，self.head和self.tail应该是一样的
+                self.head = Some(node.clone());
+                self.tail = Some(node);
+            }
+        }
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        // take方法见push_back方法中的注解
+        // 因为pop_back方法有返回值，采用Option::map的方式
+        // 比较自然，如果self.tail是None就直接返回None
+        self.tail.take().map(|node| {
+            // 判断最后一个节点有没有prev节点
+            // 如果有则断开，如果没有则把self.head和tail一起变成None
+            match node.borrow_mut().prev.take() {
+                Some(head) => {
+                    head.borrow_mut().next = None;
+                    self.tail = Some(head);
+                }
+                None => {
+                    self.head = None;
+                    self.tail = None;
+                }
+            }
+            // 这里比较关键
+            // 我们来捋捋，node是Rc类型，
+            // 表示智能指针，共享了所有权
+            // 但是pop则表示把node从双向链表中删除，即所有权转移走
+            // 我们又不知道有没有其他地方共享了所有权，所以使用Rc::try_unwrap
+            // 这个try很关键，因为编译器不知道，所以需要运行时判断
+            // 中间的ok函数表示把Result类型转成Option类型
+            // into_inner是将RefCell<T>转成T，最终所有权被释放出来了
+            Rc::try_unwrap(node).ok().unwrap().into_inner().ele
+        })
+    }
+
+    // pub fn peek_back(&self) -> Option<&T> {
+    //     self.tail.as_ref().map(|node| {
+    //         // error[E0515]: cannot return value referencing temporary value
+    //         // 这是因为node.borrow()返回了一个临时变量Ref<Node<T>>
+    //         // 所以无法返回临时变量的引用（这个也是C++中常见的问题）
+    //         &node.borrow().ele
+    //     })
+    // }
+
+    pub fn peek_back(&self) -> Option<Ref<T>> {
+        self.tail.as_ref().map(|node| {
+            // 由于node.borrow()返回的是Ref<Node<T>>
+            // 如果peek_back直接返回Ref<Node<T>>，则把内部的细节Node类型
+            // 暴露给用户，所以需要把内部细节屏蔽掉
+            // 使用Ref::map可以把内部字段映射出来
+            Ref::map(node.borrow(), |node| &node.ele)
+        })
+    }
+
+    pub fn remove_node(&mut self, node: Option<Rc<RefCell<Node<T>>>>) {
+        if let Some(n) = node {
+            let prev = n.borrow_mut().prev.take();
+            let next = n.borrow_mut().next.take();
+
+            if let Some(p) = &prev {
+                p.borrow_mut().next = next.clone();
+            } else {
+                self.head = next.clone();
+            }
+
+            if let Some(n) = &next {
+                n.borrow_mut().prev = prev;
+            } else {
+                self.tail = prev;
+            }
+        }
+    }
+
+    pub fn push_node_back(&mut self, node: Option<Rc<RefCell<Node<T>>>>) {
+        if let Some(t) = self.tail.take() {
+            if let Some(n) = &node {
+                t.borrow_mut().next = Some(n.clone());
+                n.borrow_mut().prev = Some(t);
+            }
+        } else {
+            self.head = node.clone();
+        }
+
+        self.tail = node;
+    }
+
+    pub fn pop_front_node(&mut self) -> Option<Rc<RefCell<Node<T>>>> {
+        match self.head.take() {
+            Some(head) => {
+                if let Some(n) = head.borrow_mut().next.take() {
+                    n.borrow_mut().prev = None;
+                    self.head = Some(n);
+                } else {
+                    self.head = None;
+                    self.tail = None;
+                }
+
+                Some(head)
+            }
+            None => None,
+        }
+    }
+}
+
+// 由于节点相互依赖，所以无法主动释放内存
+// 需要自定义析构函数主动释放(pop_back)所有元素
+impl<T> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        while self.pop_back().is_some() {}
+    }
+}
+
+struct LRUCache<T> {
+    cap: usize,
+    used: usize,
+    data: HashMap<i32, Rc<RefCell<Node<T>>>>,
+    list: LinkedList<T>,
+}
+
+
+impl LRUCache<(i32, i32)> {
+    fn new(capacity: i32) -> Self {
+        Self {
+            cap: capacity as usize,
+            used: 0,
+            data: HashMap::new(),
+            list: LinkedList::new(),
+        }
+    }
+
+    fn get(&mut self, key: i32) -> i32 {
+        if let Some(node) = self.data.get(&key) {
+            let val = node.borrow().ele.1;
+
+            self.list.remove_node(Some(node.clone()));
+
+            self.list.push_node_back(Some(node.clone()));
+
+            val
+        } else {
+            -1
+        }
+    }
+
+    fn put(&mut self, key: i32, value: i32) {
+        if let Some(node) = self.data.get_mut(&key) {
+            node.borrow_mut().ele = (key, value);
+
+            self.list.remove_node(Some(node.clone()));
+
+            self.list.push_node_back(Some(node.clone()))
+        } else {
+            if self.used >= self.cap {
+                if let Some(node) = self.list.pop_front_node() {
+                    self.data.remove(&node.borrow().ele.0);
+                    self.used -= 1;
+                }
+            }
+
+            let new_node = Rc::new(RefCell::new(Node::new((key, value))));
+            self.data.insert(key, new_node.clone());
+
+            self.list.push_node_back(Some(new_node));
+            self.used += 1;
+        }
+    }
+}
+
+
